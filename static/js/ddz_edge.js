190272603d041c312284c0ece6574bb1de881a17
kelaocai <kelaocai@163.com>
2014-12-13 01:50:14 +0800

    up

---
+++ static/js/ddz_edge.js
@@ -0,0 +1,13 @@
+
+(function(compId){var _=null,y=true,n=false,x15='rgba(192,192,192,1)',x5='rgba(255,255,255,0.00)',x2='5.0.0',o='opacity',e7='${Stage}',x1='5.0.1',x12='rgb(0, 0, 0)',x10='67px',h='height',x14='1',x16='true',bg='background-color',e17='${Rectangle}',x11='auto',x13='Rectangle',x9='105px',xc='rgba(0,0,0,1)',x3='5.0.1.386',w='width',tp='top',m='rect',x8='0px',lf='left',x4='rgba(207,28,28,1.00)',e6='${Ellipse}',i='none';var im='static/img/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:w,cg:i,rI:n,cn:{dom:[{id:'Ellipse',t:'ellipse',r:['317px','73px','114px','99px','auto','auto'],br:["50%","50%","50%","50%"],f:[x4],s:[0,xc,i]},{id:'Symbol_1',symbolName:'Symbol_1',t:m,r:['326px','53','105','67','auto','auto']}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'550px','400px'],overflow:'hidden',f:[x5]}}},tt:{d:2750,a:y,data:[["eid6",tp,0,697,"linear",e6,'155px','167px'],["eid8",tp,697,592,"linear",e6,'167px','60px'],["eid10",tp,1289,461,"linear",e6,'60px','125px'],["eid27",bg,2750,0,"linear",e7,'rgba(255,255,255,0.00)','rgba(255,255,255,0.00)'],["eid33",bg,2750,0,"linear",e6,'rgba(207,28,28,1.00)','rgba(207,28,28,1.00)'],["eid5",lf,0,697,"linear",e6,'106px','314px'],["eid7",lf,697,592,"linear",e6,'314px','333px'],["eid9",lf,1289,461,"linear",e6,'333px','212px']]}},"Symbol_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x8,x8,x9,x10,x11,x11],s:[0,x12,i],id:x13,o:x14,t:m,f:[x15]}],style:{'${symbolSelector}':{isStage:x16,r:[undefined,undefined,x9,x10]}}},tt:{d:2750,a:y,data:[["eid22",o,1750,0,"linear",e17,'1','1'],["eid16",tp,1750,1000,"linear",e17,'0px','113px'],["eid15",lf,1750,1000,"linear",e17,'0px','62px'],["eid14",h,1750,0,"linear",e17,'67px','67px'],["eid13",w,1750,0,"linear",e17,'105px','105px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-217536339");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+    (function(symbolName){})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+    (function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-217536339");
