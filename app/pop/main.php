824382ed1f4e38af238e6a91ab299564a68cc0e1
kelaocai <kelaocai@163.com>
2015-01-06 01:23:40 +0800

    深大活动页

--- app/pop/main.php
+++ app/pop/main.php
@@ -37,25 +37,57 @@ class main extends AWS_CONTROLLER
         TPL::output('pop/szu');
     }
 
-    public function test_action()
+    public function submit_action()
     {
         require_once("system/alipay/alipay_submit.class.php");
         require_once('system/HttpClient.class.php');
-        $key = '6E73E4BA89BA4F58A65BF644D69CA6E2';
-        $par = array('alipayId' => 'bbk@163.com', 'spm' => 'a.szu.dm', 'source' => 'weixin.dm', 'ts' => time());
-        $sort_par=argSort($par);
-        $par_link=createLinkstring($sort_par);
-        $token=md5Sign($par_link,$key);
-        $pageContents = HttpClient::quickPost('http://test.diandianzhe.com/zhe/remote/api/reg_account_ajax.htm',$par_link.'&_token='.$token);
-        echo $pageContents;
+
+        $alipay_id=$_POST['alipayid'];
+
+        if($alipay_id){
+            $key = '6E73E4BA89BA4F58A65BF644D69CA6E2';
+            $par = array('alipayId' => $alipay_id, 'spm' => 'a.szu.dm', 'source' => 'weixin.dm', 'ts' => time());
+            $sort_par=argSort($par);
+            $par_link=createLinkstring($sort_par);
+            $token=md5Sign($par_link,$key);
+            $pageContents = HttpClient::quickPost('http://test.diandianzhe.com/zhe/remote/api/reg_account_ajax.htm',$par_link.'&_token='.$token);
+//            $pageContents = HttpClient::quickPost('http://diandianzhe.com/zhe/remote/api/reg_account_ajax.htm',$par_link.'&_token='.$token);
+
+            $data=json_decode($pageContents,true);
+            echo($data['json']['code']);
+        }else{
+            echo('error');
+        }
+
+
+        //echo $pageContents;
        //echo "http://test.diandianzhe.com/zhe/remote/api/reg_account_ajax.htm?".$par_link.'&_token='.$token;
 
     }
 
+    public function test_action()
+    {
+        require_once("system/alipay/alipay_submit.class.php");
+        require_once('system/HttpClient.class.php');
+
+        $key = '6E73E4BA89BA4F58A65BF644D69CA6E2';
+        $par = array('alipayId' => 'bbcc@ddz.com', 'spm' => 'a.szu.dm', 'source' => 'weixin.dm', 'ts' => time());
+        $sort_par=argSort($par);
+        $par_link=createLinkstring($sort_par);
+        $token=md5Sign($par_link,$key);
+        $pageContents = HttpClient::quickPost('http://test.diandianzhe.com/zhe/remote/api/reg_account_ajax.htm',$par_link.'&_token='.$token);
+        $data=json_decode($pageContents,true);
+//        echo($data['json']['code']);
+        echo $pageContents;
+
+
+    }
+
+
     public function mn_action(){
         $json='{"json":{"code":"success","success":true}}';
         $data=json_decode($json,true);
-        echo($data['json']['code']);
+        echo($data['json']['success']);
     }
 
 
